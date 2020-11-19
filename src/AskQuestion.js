fa05f4b86510d051d8c926de1ff96ff3038ca9f2
Sarthak Agrawal <sarthavader@gmail.com>
2020-11-18 19:45:59 -0600

    setup post api call

--- src/AskQuestion.js
+++ src/AskQuestion.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 
 class AskQuestion extends Component {
+    state = {
+        question: null
+    }
+    componentDidMount() {
+        fetch('http://127.0.0.1:8000/api/v1/questions/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                content: "This is a question from the frontend",
+                time_asked: "2020-11-13T16:30:45.421158Z",
+                email: "person@example.com",
+                has_answer: false,
+            })
+        })
+    }
+
     render() {
         return (
             <div>
